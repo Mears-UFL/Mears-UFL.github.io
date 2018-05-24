cfc2ab304acb1e3aad8a76e7a597c4eb6305a9d2
Mears-UFL <16980450+Mears-UFL@users.noreply.github.com>
2018-05-24 14:52:15 -0400

    Updated org locations Boston, GNV

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,11 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Boston MA, USA",
+    42.3354332,
+    -71.1043608
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
-];
+    "Gainesville, FL",
+    @29.6433786,
+    -82.3469519
+  ];
